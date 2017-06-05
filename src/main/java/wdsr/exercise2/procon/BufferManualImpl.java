1256da30293dec32d7169599c6240b631ff0cce8
biedro94 <biedrzyckid94@gmail.com>
2017-06-05 22:24:13 +0200

    Solution exercise 2 procon

--- src/main/java/wdsr/exercise2/procon/BufferManualImpl.java
+++ src/main/java/wdsr/exercise2/procon/BufferManualImpl.java
@@ -1,16 +1,52 @@
 package wdsr.exercise2.procon;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * Task: implement Buffer interface without using any *Queue classes from java.util.concurrent package.
  * Any combination of "synchronized", *Lock, *Semaphore, *Condition, *Barrier, *Latch is allowed.
  */
 public class BufferManualImpl implements Buffer {
+	
+	private Queue<Order> queueOrders = new LinkedList<Order>();
+	private Lock lock = new ReentrantLock();
+	private Condition isNotFull = lock.newCondition();
+	private Condition isNotEmpty = lock.newCondition();
+	private boolean access =true;
+	
 	public void submitOrder(Order order) throws InterruptedException {
-		// TODO
+		lock.lock();
+		
+		while(!access){
+			isNotEmpty.await();
+		}
+		
+		queueOrders.add(order);
+		isNotFull.signal();
+		access = false;
+		lock.unlock();
 	}
 	
 	public Order consumeNextOrder() throws InterruptedException {
-		// TODO
-		return null;
+		Order order;
+		
+		lock.lock();
+		
+		while(access){
+			isNotFull.await();
+		}
+		
+		order = queueOrders.remove();
+		isNotEmpty.signal();
+		access=true;
+		lock.unlock();
+		
+		
+		
+		return order;
 	}
 }
--- src/main/java/wdsr/exercise2/procon/BufferQueueImpl.java
+++ src/main/java/wdsr/exercise2/procon/BufferQueueImpl.java
@@ -1,15 +1,19 @@
 package wdsr.exercise2.procon;
 
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 /**
  * Task: implement Buffer interface using one of *Queue classes from java.util.concurrent package.
  */
 public class BufferQueueImpl implements Buffer {
+	
+	private BlockingQueue<Order> queue = new LinkedBlockingQueue<Order>();
+	
 	public void submitOrder(Order order) throws InterruptedException {
-		// TODO
+		queue.add(order);
 	}
 	
 	public Order consumeNextOrder() throws InterruptedException {
-		// TODO
-		return null;
+		return queue.take();
 	}
 }
